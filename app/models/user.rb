08ad2bccf5e17661ca9c357eb957291333e99fd8
jrdegreeff <jrdegreeff@gmail.com>
2020-04-29 04:41:10 -0400

    fix linter issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  include PgSearch
+  include PgSearch::Model
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable,
