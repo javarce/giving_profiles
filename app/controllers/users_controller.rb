07bc2d0ad43bbb4999c9194ef091a8c99485fedc
jrdegreeff <jrdegreeff@gmail.com>
2020-05-10 23:28:03 -0400

    add 403 page
    [#18]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -87,8 +87,6 @@ class UsersController < ApplicationController
   end
 
   def render_forbidden
-    # TODO: add 403 page and render it
-    # render file: 'public/403.html', status: 403
-    redirect_to root_path
+    render file: 'public/403.html', status: 403, layout: false
   end
 end
