7c0bbd5a3e469f51340ee3262fd27415a2648d14
jrdegreeff <jrdegreeff@gmail.com>
2020-05-26 15:20:16 -0400

    Completely overhaul past donations section of profile
    [#17]
    
    Still need to add functionality to action buttons -- see [#43]

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( shared/donation_bars.js shared/flash.js )
+Rails.application.config.assets.precompile += %w( shared/donation_bars.js shared/flash.js users/profile/past_donations.js)
